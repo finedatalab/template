784a4f1051726f31aa9488762034f8bf172ea82e
hskim <hskim@finedatalab>
2020-04-13 16:40:56 +0900

    BigQuery demo1

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, g, request, Response, make_response, session
-from flask import render_template, Markup
+import flask 
 from datetime import datetime, date, timedelta
+from google.cloud import bigquery
+import concurrent.futures
+
 
 try:
   import googleclouddebugger
@@ -8,8 +10,9 @@ try:
 except ImportError:
   pass
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.debug = True
+bigquery_client = bigquery.Client()
 
 
 app.config.update(
@@ -27,23 +30,23 @@ def set_response_headers(r):
 
 @app.route("/")
 def main():
-    return render_template("index.html", document_title="Analytics Dashboard")
+    return flask.render_template("index.html", document_title="Analytics Dashboard")
 
 @app.route("/dashboard-social")
 def dashboard():
-    return render_template("dashboard-social.html", document_title="Social Wall")
+    return flask.render_template("dashboard-social.html", document_title="Social Wall")
 
 @app.route("/layouts")
 def layouts():
-    return render_template("layouts.html",  document_title="App Layouts")
+    return flask.render_template("layouts.html",  document_title="App Layouts")
 
 @app.route("/skins")
 def skins():
-    return render_template("skins.html", document_title="Prebuilt Skins")
+    return flask.render_template("skins.html", document_title="Prebuilt Skins")
 
 @app.route("/applayout")
 def applayout():
-    return render_template("applayout.html", document_title="App Settings")
+    return flask.render_template("applayout.html", document_title="App Settings")
 
 @app.route("/inbox")
 def inbox():
@@ -52,43 +55,43 @@ def inbox():
 
 @app.route("/flot")
 def flot():
-    return render_template("flot.html", document_title="Flot Chart")
+    return flask.render_template("flot.html", document_title="Flot Chart")
 
 @app.route("/morris")
 def morris():
-    return render_template("morris.html", document_title="Morris Charts")
+    return flask.render_template("morris.html", document_title="Morris Charts")
 
 @app.route("/sparkline-charts")
 def sparkline():
-    return render_template("sparkline-charts.html", document_title="Sparklines")
+    return flask.render_template("sparkline-charts.html", document_title="Sparklines")
 
 @app.route("/easypie-charts")
 def easypie():
-    return render_template("easypie-charts.html", document_title="EasyPieCharts")
+    return flask.render_template("easypie-charts.html", document_title="EasyPieCharts")
 
 @app.route("/dygraphs")
 def dygraphs():
-    return render_template("dygraphs.html", document_title="Dygraphs")
+    return flask.render_template("dygraphs.html", document_title="Dygraphs")
 
 @app.route("/chartjs")
 def chartjs():
-    return render_template("chartjs.html", document_title="Chart.js")
+    return flask.render_template("chartjs.html", document_title="Chart.js")
 
 @app.route("/hchartable")
 def hchartable():
-    return render_template("hchartable.html", document_title="HighchartTable")
+    return flask.render_template("hchartable.html", document_title="HighchartTable")
 
 @app.route("/table")
 def table():
-    return render_template("table.html", document_title="Normal Tables")
+    return flask.render_template("table.html", document_title="Normal Tables")
 
 @app.route("/datatables")
 def datatables():
-    return render_template("datatables.html", document_title="Data Tables")
+    return flask.render_template("datatables.html", document_title="Data Tables")
 
 @app.route("/jqgrid")
 def jqgrid():
-    return render_template("jqgrid.html", document_title="Jquery Grid")
+    return flask.render_template("jqgrid.html", document_title="Jquery Grid")
 
 @app.route("/form-elements")
 def formelements():
@@ -96,143 +99,143 @@ def formelements():
 
 @app.route("/form-templates")
 def formtemplates():
-    return render_template("form-templates.html", document_title="Smart Form Layouts")
+    return flask.render_template("form-templates.html", document_title="Smart Form Layouts")
 
 @app.route("/validation")
 def validation():
-    return render_template("validation.html", document_title="Smart Form Validation")
+    return flask.render_template("validation.html", document_title="Smart Form Validation")
 
 @app.route("/bootstrap-forms")
 def bootstrapforms():
-    return render_template("bootstrap-forms.html", document_title="Bootstrap Form Elements")
+    return flask.render_template("bootstrap-forms.html", document_title="Bootstrap Form Elements")
 
 @app.route("/bootstrap-validator")
 def bootstrapvalidator():
-    return render_template("bootstrap-validator.html", document_title="Bootstrap Form Validation")
+    return flask.render_template("bootstrap-validator.html", document_title="Bootstrap Form Validation")
 
 @app.route("/plugins")
 def plugins():
-    return render_template("plugins.html", document_title="Form Plugins")
+    return flask.render_template("plugins.html", document_title="Form Plugins")
 
 @app.route("/wizard")
 def wizard():
-    return render_template("wizard.html", document_title="Wizards")
+    return flask.render_template("wizard.html", document_title="Wizards")
 
 @app.route("/other-editors")
 def othereditors():
-    return render_template("other-editors.html", document_title="Bootstrap Editors")
+    return flask.render_template("other-editors.html", document_title="Bootstrap Editors")
 
 @app.route("/dropzone")
 def dropzone():
-    return render_template("dropzone.html", document_title="Dropzone")
+    return flask.render_template("dropzone.html", document_title="Dropzone")
 
 @app.route("/image-editor")
 def imageeditor():
-    return render_template("image-editor.html", document_title="Image Cropping")
+    return flask.render_template("image-editor.html", document_title="Image Cropping")
 
 @app.route("/ckeditor")
 def ckeditor():
-    return render_template("ckeditor.html", document_title="CK Editor")
+    return flask.render_template("ckeditor.html", document_title="CK Editor")
 
 @app.route("/general-elements")
 def generalelements():
-    return render_template("general-elements.html", document_title="General Elements")
+    return flask.render_template("general-elements.html", document_title="General Elements")
 
 @app.route("/buttons")
 def buttons():
-    return render_template("buttons.html", document_title="Buttons")
+    return flask.render_template("buttons.html", document_title="Buttons")
 
 @app.route("/fa")
 def fa():
-    return render_template("fa.html", document_title="Font Awesome")
+    return flask.render_template("fa.html", document_title="Font Awesome")
 
 @app.route("/glyph")
 def glyph():
-    return render_template("glyph.html", document_title="Glyph Icons")
+    return flask.render_template("glyph.html", document_title="Glyph Icons")
 
 @app.route("/flags")
 def flags():
-    return render_template("flags.html", document_title="Flags")
+    return flask.render_template("flags.html", document_title="Flags")
 
 @app.route("/grid")
 def grid():
-    return render_template("grid.html", document_title="Grid")
+    return flask.render_template("grid.html", document_title="Grid")
 
 @app.route("/treeview")
 def treeview():
-    return render_template("treeview.html", document_title="Tree View")
+    return flask.render_template("treeview.html", document_title="Tree View")
 
 @app.route("/nestable-list")
 def nestablelist():
-    return render_template("nestable-list.html", document_title="Nestable Lists")
+    return flask.render_template("nestable-list.html", document_title="Nestable Lists")
 
 @app.route("/jqui")
 def jqui():
-    return render_template("jqui.html", document_title="JQuery UI")
+    return flask.render_template("jqui.html", document_title="JQuery UI")
 
 @app.route("/typography")
 def typography():
-    return render_template("typography.html", document_title="Typography")
+    return flask.render_template("typography.html", document_title="Typography")
 
 @app.route("/widgets")
 def widgets():
-    return render_template("widgets.html", document_title="Widgets")
+    return flask.render_template("widgets.html", document_title="Widgets")
 
 @app.route("/calendar")
 def calendar():
-    return render_template("calendar.html", document_title="Calendar")
+    return flask.render_template("calendar.html", document_title="Calendar")
 
 @app.route("/gmap-xml")
 def gmapxml():
-    return render_template("gmap-xml.html", document_title="GMap Skins")
+    return flask.render_template("gmap-xml.html", document_title="GMap Skins")
 
 @app.route("/projects")
 def projects():
-    return render_template("projects.html", document_title="Projects")
+    return flask.render_template("projects.html", document_title="Projects")
 
 @app.route("/blog")
 def blog():
-    return render_template("blog.html", document_title="Blog")
+    return flask.render_template("blog.html", document_title="Blog")
 
 @app.route("/gallery")
 def gallery():
-    return render_template("gallery.html", document_title="Gallery")
+    return flask.render_template("gallery.html", document_title="Gallery")
 
 @app.route("/forum")
 def forum():
-    return render_template("forum.html", document_title="General View")
+    return flask.render_template("forum.html", document_title="General View")
 
 @app.route("/forum-topic")
 def forumtopic():
-    return render_template("forum-topic.html", document_title="Topic View")
+    return flask.render_template("forum-topic.html", document_title="Topic View")
 
 @app.route("/forum-post")
 def forumpost():
-    return render_template("forum-post.html", document_title="Post View")
+    return flask.render_template("forum-post.html", document_title="Post View")
 
 @app.route("/profile")
 def profile():
-    return render_template("profile.html", document_title="Profile")
+    return flask.render_template("profile.html", document_title="Profile")
 
 @app.route("/timeline")
 def timeline():
-    return render_template("timeline.html", document_title="Timeline")
+    return flask.render_template("timeline.html", document_title="Timeline")
 
 @app.route("/search")
 def search():
-    return render_template("search.html", document_title="Search Page")
+    return flask.render_template("search.html", document_title="Search Page")
 
 @app.route("/orders")
 def orders():
-    return render_template("orders.html", document_title="Orders")
+    return flask.render_template("orders.html", document_title="Orders")
 
 @app.route("/products-view")
 def productsview():
-    return render_template("products-view.html", document_title="Products View")
+    return flask.render_template("products-view.html", document_title="Products View")
 
 @app.route("/products-detail")
 def productsdetail():
-    return render_template("products-detail.html", document_title="Products Detail")
+    return flask.render_template("products-detail.html", document_title="Products Detail")
 
 @app.route("/Landing_Page")
 def Landing_Page():
@@ -240,39 +243,82 @@ def Landing_Page():
 
 @app.route("/pricing-table")
 def pricingtable():
-    return render_template("pricing-table.html", document_title="Pricing Tables")
+    return flask.render_template("pricing-table.html", document_title="Pricing Tables")
 
 @app.route("/invoice")
 def invoice():
-    return render_template("invoice.html", document_title="Invoice")
+    return flask.render_template("invoice.html", document_title="Invoice")
 
 @app.route("/login")
 def login():
-    return render_template("login.html", document_title="Login")
+    return flask.render_template("login.html", document_title="Login")
 
 @app.route("/register")
 def register():
-    return render_template("register.html", document_title="Register")
+    return flask.render_template("register.html", document_title="Register")
 
 @app.route("/forgotpassword")
 def forgotpassword():
-    return render_template("forgotpassword.html", document_title="Forgot Password")
+    return flask.render_template("forgotpassword.html", document_title="Forgot Password")
 
 @app.route("/lock")
 def lock():
-    return render_template("lock.html", document_title="Locked Screen")
+    return flask.render_template("lock.html", document_title="Locked Screen")
 
 @app.route("/error404")
 def error404():
-    return render_template("error404.html", document_title="Error 404")
+    return flask.render_template("error404.html", document_title="Error 404")
 
 @app.route("/error500")
 def error500():
-    return render_template("error500.html", document_title="Error 500")
+    return flask.render_template("error500.html", document_title="Error 500")
 
 @app.route("/blank_")
 def blank_():
-    return render_template("blank_.html", document_title="Blank Page")
+    return flask.render_template("blank_.html", document_title="Blank Page")
+
+@app.route("/BigQuery-demo1")
+def BigQuerydemo1():
+    query_job = bigquery_client.query(
+        """
+        SELECT name, SUM(number) as total_people
+        FROM `bigquery-public-data.usa_names.usa_1910_2013`
+        WHERE state = 'TX'
+        GROUP BY name, state
+        ORDER BY total_people DESC
+        LIMIT 20
+    """
+    )       
+
+    return flask.redirect(
+        flask.url_for(
+            "results",
+            project_id=query_job.project,
+            job_id=query_job.job_id,
+            location=query_job.location,
+        )
+    )
+
+@app.route("/results")
+def results():
+    project_id = flask.request.args.get("project_id")
+    print(project_id)
+    job_id = flask.request.args.get("job_id")
+    location = flask.request.args.get("location")
+
+    query_job = bigquery_client.get_job(
+        job_id,
+        project=project_id,
+        location=location,
+    )
+
+    try:
+        # Set a timeout because queries could take longer than one minute.
+        query_results = query_job.result(timeout=30)
+    except concurrent.futures.TimeoutError:
+        return flask.render_template("timeout.html", job_id=query_job.job_id)
+
+    return flask.render_template("BigQuery-demo1-result.html", results=query_results )
 
 
 
